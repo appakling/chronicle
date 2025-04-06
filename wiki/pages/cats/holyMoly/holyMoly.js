932c0e6405dc5be49128b43a0559fbfc1f56e483
appakling <84096188+appakling@users.noreply.github.com>
2025-04-06 16:31:43 +0000

    working on that js multi line issue.

--- wiki/pages/cats/holyMoly/holyMoly.js
+++ wiki/pages/cats/holyMoly/holyMoly.js
@@ -1,22 +1,6 @@
-// to be honest this is stolen from stack overflow just like indexscript.js, don't judge me pls
+const textToDisplay = 'Basil';  // The specific text you want to display
 
-const textSelect= [ // our table
-    'Basil',
-    'Also Basil'
-    
-];
- 
-function getRandomText() {
-    const randomIndex = Math.floor(Math.random() * textSelect.length); //do some math 
-    return textSelect[randomIndex];
-  }
- 
 function loadText() {
-  const myDiv = document.getElementById('name'); // find an html element with our "phrase" id in it
-  myDiv.innerHTML= getRandomText(); // load our random text into this element
+  const myDiv = document.getElementById('name'); // Find the HTML element with id "name"
+  myDiv.innerHTML = textToDisplay; // Load the specific text into the element
 }
- 
-window.onload = loadText; // load in our text when the window loads
-
-//example: <p id="phrase"></p>
-// this works with any text :D
