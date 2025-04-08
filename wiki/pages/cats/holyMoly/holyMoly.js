9c8c75a364738844ea4ff21ae495670d831b3102
appakling <84096188+appakling@users.noreply.github.com>
2025-04-07 19:55:04 -0500

    commits broke 😭

--- wiki/pages/cats/holyMoly/holyMoly.js
+++
@@ -1,8 +0,0 @@
-const textToDisplay = 'Basil';  // The specific text you want to display
-
-function loadText() {
-  const myDiv = document.getElementById('name'); // Find the HTML element with id "name"
-  myDiv.innerHTML = textToDisplay; // Load the specific text into the element
-}
-
-window.onload = loadText; // load in our text when the window loads
